e9796f83717a7a0acb915c1796085ebf097e3e37
Nalin Anand <nalinanand11974@gmail.com>
2026-09-03 11:44:39 +0530

    Add files via upload

---
+++ day23_code2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int i,j; // Declares two integer variables for controlling the loops
+for(i=1;i<=5;i++) // Loops 5 times to print 5 rows
+{
+for(j=1;j<=5;j++) // Loops 5 times to print 5 stars in each row
+{
+printf("* "); // Prints a star followed by a space
+}
+printf("\n"); // Moves the cursor to the next line after each row
+}
+return 0;
+}
+
+
+
+
