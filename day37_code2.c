c15fdbdf39325a6c1c5bfe4b176888e8a5db40ad
Nalin Anand <nalinanand11974@gmail.com>
2026-09-15 13:13:29 +0530

    Add files via upload

---
+++ day37_code2.c
@@ -0,0 +1,53 @@
+// Q74: Find the transpose of a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+1 4
+2 5
+3 6
+
+*/
+
+#include<stdio.h>
+#define MAX_ROWS 10
+#define MAX_COLS 10
+int main(void)
+{
+int i,j,rows,columns;
+int matrix[MAX_ROWS][MAX_COLS],transpose[MAX_COLS][MAX_ROWS];
+scanf("%d %d",&rows,&columns);
+if(rows<1||rows>MAX_ROWS||columns<1||columns>MAX_COLS)
+{
+printf("Invalid matrix size.\n");
+return 1;
+}
+for(i=0;i<rows;i++)
+{
+for(j=0;j<columns;j++)
+{
+scanf("%d",&matrix[i][j]);
+}
+}
+for(i=0;i<rows;i++)
+{
+for(j=0;j<columns;j++)
+{
+transpose[j][i]=matrix[i][j];
+}
+}
+printf("\n");
+for(i=0;i<columns;i++)
+{
+for(j=0;j<rows;j++)
+{
+printf("%2d",transpose[i][j]);
+}
+printf("\n");
+}
+return 0;
+}
